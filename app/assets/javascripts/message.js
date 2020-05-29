19f0d949385a55310f764ac6a42833d440020f0d
yu-su8016 <63726642+yu-su8016@users.noreply.github.com>
2020-05-29 11:34:15 +0900

    Asynchronous-commit

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,63 @@
+$(function(){ 
+      function buildHTML(message){
+      if ( message.image ) {
+        var html =
+          `<div class="chat-main__message__list">
+            <div class="chat-main__message__list__info">
+              <div class="chat-main__message__list__info--name">
+                ${message.user_name}
+              </div>
+              <div class="chat-main__message__list__info--date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="chat-main__message__lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+          `<div class="chat-main__message__list">
+            <div class="chat-main__message__list__info">
+              <div class="chat-main__message__list__info--name">
+                ${message.user_name}
+              </div>
+              <div class="chat-main__message__list__info--date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="chat-main__message__lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+        };
+      }
+$('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.chat-main__message').append(html);
+        $('.btn-submit').prop('disabled', false);
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+    });
+})
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,9 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -18,7 +20,6 @@ class MessagesController < ApplicationController
   end
 
   private
-
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
